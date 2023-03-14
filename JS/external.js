4386b47c2d29e987892669e6e62a119b2bd247e2
melchavezg <mchavezg300@gmail.com>
2023-03-14 00:07:42 -0500

    commiting

---
+++ JS/external.js
@@ -0,0 +1,32 @@
+console.log("Hello from external Javascript")
+
+alert("Welcome to my website!");
+var userInput = prompt('What is your favorite color?');
+alert('Great, ' + userInput + ' is mine too!!! ');
+
+prompt('You rented the Little Mermaid for 3 days, Brother Bear for 5 days, and Hercules for 1 day. if it costs $3 per movie per day, how much did you spend?');
+var littleMermaid = 3*3
+var brotherBear = 5*3
+var hercules = 1*3
+alert('In total you spent $' + (littleMermaid + brotherBear + hercules) + '!')
+
+prompt(' If you worked 10 hrs at Facebook with rate of $350hr, Google 6 hours for $400hr, and Amazon 4 hours for $380, how much would you make this week?');
+var faceBook = 10*350
+var google = 6*400
+var amazon = 4*380
+alert('You would make $' +  faceBook + google + amazon + ' this week!')
+
+prompt("If class is not full would you like to attend?")
+alert("Thank you for your response")
+
+prompt('Are you a member?')
+
+
+
+
+
+
+
+
+
+
