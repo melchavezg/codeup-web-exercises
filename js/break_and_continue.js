7fd77b963f811d271dd40f9f2660d642845b328d
melchavezg <mchavezg300@gmail.com>
2023-03-28 15:41:40 -0500

    finished

---
+++ js/break_and_continue.js
@@ -0,0 +1,15 @@
+prompt("Pick a number between 1- 50!")
+var numberToStopAt = 5;
+
+for (var i = 1; i < 100; i++) {
+
+    console.log('Loop counter is: ' + i);
+
+    if (i === numberToStopAt) {
+        console.log('We have reached the stopping point: break!');
+        // use the break keyword to exit from the while loop
+        break;
+        // nothing after the break will get processed
+        console.log('You will never see this line.');
+    }
+}
